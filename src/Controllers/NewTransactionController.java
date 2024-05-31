94bb60d7a1421f5bc653b286b6637af764cf7e60
RobertBannoura <bannourarobert@gmail.com>
2024-05-31 19:56:53 +0300

    Robert

---
+++ src/Controllers/NewTransactionController.java
@@ -0,0 +1,45 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class NewTransactionController {
+
+    @FXML
+    private ComboBox<?> ComboxCompany;
+
+    @FXML
+    private Button Insert;
+
+    @FXML
+    private TextField txtAmount;
+
+    @FXML
+    private TextField txtID;
+
+    @FXML
+    private TextField txtModel;
+
+    @FXML
+    private TextField txtPurchaseDate;
+
+    @FXML
+    private TextField txtPurchaseDate1;
+
+    @FXML
+    private TextField txtQuantity;
+
+    @FXML
+    void ComboxCompany(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addEmployee(ActionEvent event) {
+
+    }
+
+}
